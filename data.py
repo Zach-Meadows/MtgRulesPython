d24d9ccd65787b0be99439cf9baa81993492757f
Zach Meadows <zmeadows1@gmail.com>
2019-11-28 21:48:31 -0500

    regex working

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-
+import re
 # beautiful soup web scraping template
 
 my_url = 'https://media.wizards.com/2019/downloads/MagicCompRules%2020191004.txt'
@@ -11,5 +11,12 @@ page_html = uClient.read()
 uClient.close()
 
 page_soup = soup(page_html, "html.parser")
+# convert webpage to string
+string = str(page_soup)
+# a regex to find a single number followed
+# by a period (ex. 1. and not 100.1.)
+numberPeriod = r"((?<!...)\b[0-9]\. )"
+# use regex to list all numbers (soon)
+rules = re.split(numberPeriod, string)
+print (rules[0])
 
-print (page_soup)
